4664c74e0ece608b22d480ab2c3c22b74df262b2
KIMMyeongJong <32411372+KIMMyeongJong@users.noreply.github.com>
2018-04-18 19:26:48 +0900

    Add files via upload

---
+++ 20180418.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 김명종
+ * Date: 2018-04-18
+ * Time: 오후 6:23
+ */
+echo "KIM Myeong Jong";
+for ($i = 0; $i<10; $i++){
+    echo "Today ",date("ymd | H:i:s");
+    echo "<br>";
+}
+//for문은 자료형[ex)int]이 아닌 $로 구별된다.
+?>
